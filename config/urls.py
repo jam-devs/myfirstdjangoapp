27534d2c0b5b7da8eb0e6f73f32246835dfcd4f1
jam-devs <meljamtorrecampo@gmail.com>
2025-09-19 21:45:30 +0800

    week1 setup - venv, Django, config project

--- config/urls.py
+++ config/urls.py
@@ -16,7 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from core.views import about, home  # remove 'base'
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', home, name='home'),
+    path('about/', about, name='about'),
 ]
+
